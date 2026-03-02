0012738a738d1e2f8b4ddea4ccde025263aa35ce
HubertKlo <hubert.klosekk@gmail.com>
2026-03-02 13:20:02 +0100

    Połączenie każdego punktu z każdym
    pasuje to zmienic i wybierac kiedy chcemy co

--- main.cpp
+++ main.cpp
@@ -1,44 +1,69 @@
 #include <SDL2/SDL.h>
+#include <list>
 #include <iostream>
-
+struct point
+{
+    float x;
+    float y;
+    point(){
+        x=0;
+        y=0;
+    }
+    point(float a, float b){
+        x=a;
+        y=b;
+    }
+};
 int main(int argc, char* argv[])
 {
-    int rendererFlags, windowFlags;
-    int SCREEN_WIDTH = 500, SCREEN_HEIGHT = 500;
+    std::list<point> points;
+    bool quit = false;
+    SDL_Event event;
+    point p1(1,2);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("SDL2",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+    while(!quit){
+        while(SDL_PollEvent(&event)){
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            
+            case SDL_MOUSEBUTTONUP:
+            switch (event.button.button) {
+                case SDL_BUTTON_LEFT:
+                points.push_front(point(event.button.x,event.button.y));
+            break;
+            case SDL_BUTTON_RIGHT:
+                for(auto it:points){
+                    std::cout<<"x "<<it.x<<"y "<<it.y<<std::endl;
+                }
+            break;
+            }
+         }
+         
+        SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+        SDL_RenderClear(renderer);
 
-    rendererFlags = SDL_RENDERER_ACCELERATED;
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        for (auto it : points) {
+            SDL_RenderDrawPointF(renderer, it.x, it.y);
+            for(auto ita : points){
+             SDL_RenderDrawLineF(renderer, it.x, it.y,ita.x,ita.y);
+            }
+        }
 
-    windowFlags = 0;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    SDL_Window*  window = SDL_CreateWindow("Hoe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
-
-    if (!window)
-    {
-        printf("Failed to open %d x %d window: %s\n", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_GetError());
-        exit(1);
-    }
-
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, rendererFlags);
-
-    SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderDrawLine(renderer, 10, 10, 250, 250);
     SDL_RenderPresent(renderer);
-
-	int a;
-	std::cin >> a;
-
+    }   
+}
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-
-	return 0;
+    points.~list();
+    SDL_Quit();
+ 
+    return 0;
 }
